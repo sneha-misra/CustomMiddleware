1d8a63ed8c117c3e0968a7e91ca79ba4d65ff1a2
sneha-misra <142022086+sneha-misra@users.noreply.github.com>
2023-08-22 16:49:48 +0530

    Add files via upload

---
+++ CustomTestMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomTestMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CustomTestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync("- Before Message -  \n\r");
+            await _next(context);
+            await context.Response.WriteAsync("\n\r - After Message - ");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class WTRCustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomTestMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomTestMiddleware>();
+        }
+    }
+}
